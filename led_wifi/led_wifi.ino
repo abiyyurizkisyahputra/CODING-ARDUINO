2e175f36e95520254ceac0da8b5e0ec035610e1a
abiyyurizkisyahputra <87510759+abiyyurizkisyahputra@users.noreply.github.com>
2021-09-29 01:07:36 +0700

    Add files via upload

---
+++ led_wifi/led_wifi.ino
@@ -0,0 +1,13 @@
+#include <WiFiLink.h>
+
+#include <WiFi.h>;
+ 
+const char* ssid = "RobotikID";
+const char* password = "robotikid";
+
+WiFi.begin(ssid, password);
+
+while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+}
